8b01eecda4a64d2eff45035d92d6c20c370fda4f
JzuvGTI <70012176+JzuvGTI@users.noreply.github.com>
2021-08-08 08:40:36 -0700

    Create Message.cpp

---
+++ Message.cpp
@@ -0,0 +1,9 @@
+{
+"Version": 5,
+"Creator": "JzuvGTI",
+"Title": "UPDATE !!",
+"Info": "- FIXED MULTIBOT DC EVERY 5 MIN",
+"Changelogs": "↓↓ Link Here ↓↓",
+"Link": "https://www.mediafire.com/file/3kj1m8it9ufq9z9/%255B_Fix_Multibot_3.67_%255D_Growtopia_3.67.apk/file",
+"Premium": true
+}
